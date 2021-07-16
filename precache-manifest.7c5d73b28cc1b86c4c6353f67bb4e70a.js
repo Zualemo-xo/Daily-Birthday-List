36c13e9cc7663575a62255f3f56eef965e57dd63
Zualemo-xo <maheysh.v2019@vitstudent.ac.in>
2021-07-17 03:55:31 +0530

    Updates

---
+++ precache-manifest.7c5d73b28cc1b86c4c6353f67bb4e70a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b947f79dfcff13218c39b48edab6d7e9",
+    "url": "/Daily-Birthday-List/index.html"
+  },
+  {
+    "revision": "78191e2d3ac78cad04e0",
+    "url": "/Daily-Birthday-List/static/css/main.b96d8257.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/Daily-Birthday-List/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Daily-Birthday-List/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78191e2d3ac78cad04e0",
+    "url": "/Daily-Birthday-List/static/js/main.2d7083a3.chunk.js"
+  },
+  {
+    "revision": "1590e8139c259f1e3ef6",
+    "url": "/Daily-Birthday-List/static/js/runtime-main.f57765aa.js"
+  }
+]);
